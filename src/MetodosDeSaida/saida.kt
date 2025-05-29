595cce5b8637b513c70eea8bbb7968564ae710b3
Márlon <mo14421890@gmail.com>
2025-05-29 20:27:30 -0300

    Uptade files

---
+++ src/MetodosDeSaida/saida.kt
@@ -0,0 +1,11 @@
+package MetodosDeSaida
+
+fun main(){
+    println("Ola!!") // coamando print serve como o metodo de saida, junto da sigla ln serve como quebra de linha
+    println("Ola!!")
+
+    println() //Usei para pular uma linha apenas
+
+    print("ola")
+    print("ola")// observa-se que o apenas o comando "print" sem as siglas "ln" não tem quebra de linha
+}
--- src/Variaveis/TiposDeVariaveis.kt
+++ src/Variaveis/TiposDeVariaveis.kt
@@ -1,15 +1,38 @@
 package Variaveis
-
+//variavel do tipo Int(inteiro)
 fun variaveis(){
-    //declarar uma variavel definindo seu tipo!
-    val numero: Int = 10
-    //declarar uma variavael sem precisar definir seu tipo!
-    val numer = 10
-    //mostrar valores, o cifrao($)e usado para colocar uma variavel em uma string
-    println("$numero e $numer")
-    //soma de variaveis
-    println(numero + numer)
+
+    val numero: Int = 10 //declarar uma variavel definindo seu tipo!
+
+    val numer = 10  //declarar uma variavael sem precisar definir seu tipo!
+
+    println("$numero e $numer") //mostrar valores, o cifrao($)e usado para colocar uma variavel em uma string
+
+    println(numero + numer)  //soma de variaveis
+
+    println()
+
+//variaveis do tipo String(texto)
+
+    val texto: String = "Olá" //declarando o tipo da variavel
+    val texto2 = "mundo" // sem precisar declarar o tipo da variavel
+
+    println("$texto $texto2")
+
+    println()
+// variaveis do tipo Char(Caractere)
+
+    val caracter: Char = 'O'//observa-se que todo caracter fica dentro de aspas simples ''
+    val caracter2: Char = 'l'
+    val caracter3: Char = 'á'
+    println("$caracter$caracter2$caracter3")//sera imprimida na tela a palavra "Ola"
+
+    println()
+
 }
+
+
+
 
 
 
